4be362acd6a10d7ae2776111b7717548a6959550
Samuel Malec <98909801+S3MU1L@users.noreply.github.com>
2022-05-21 21:20:13 +0200

    Update uloha2.java

--- opakovanie/sada10/uloha2.java
+++ opakovanie/sada10/uloha2.java
@@ -1,40 +1,35 @@
 package sada10;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class uloha2 {
+public class lopticka {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner x = new Scanner(System.in);
-		System.out.println("Zadaj vysku a k oddelene ciarkou:");
 		double h = x.nextDouble();
-		DecimalFormat df = new DecimalFormat("0.00");
 		double k = x.nextDouble();
-		//n je pocet krat, kolko lopta skoci nad 1m.
-		
-		// uvedomim si, ze h *k^n >=1
-		// kde k^n nie je xor ale k na ntu;
-		// dam limitny pripad : h * k^n = 1
-		//predelim h
-		// zlogaritmujem
-		//dostavam --> log o zaklade k z (1/h) = n
-		//kedze v programovani nemame logaritmy o 'cudnych zakladoch', musime si vystacit so vzorcom
-		// ze logn(k) = log10(k)/log10(n), nemusi byt zaklad 10, moze byt hociaky iny, len musia byt rovnake 
-		int n = (int) (Math.log10(1/h)/Math.log10(k));
-		System.out.println(n);
-		for (int i = 0; i <=10; i++) {
-			if (h >= 1) {
-				System.out.println(i+": "+df.format(h) + "m");
-			}
-			else {
-				
-				
-				System.out.println(i+": "+df.format(100*h)+ " cm");
+		int nad = 0;
+		ArrayList<Double>vysky = new ArrayList<>();
+		DecimalFormat df = new DecimalFormat("0.00");
+		for (int i = 0; i < 11; i++) {
+			vysky.add(h);
+			if (h >= 1 && i != 0) {
+				++nad;
 			}
 			h *= k;
-			
+		}
+		System.out.println(nad);
+		for (int i = 0; i < vysky.size(); i++) {
+			String jednotka = "m";
+			double c = vysky.get(i);
+			if (c < 1) {
+				 c*= 100;
+				 jednotka = "cm";
+			}
+			System.out.println(i+": " + df.format(c)+" "+ jednotka);
 		}
 	}
 
