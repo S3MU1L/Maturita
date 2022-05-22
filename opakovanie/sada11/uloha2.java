c2c2fc6835237e7b13d801e3c07265a6549b2710
Samuel Malec <98909801+S3MU1L@users.noreply.github.com>
2022-05-22 12:24:03 +0200

    Update uloha2.java

--- opakovanie/sada11/uloha2.java
+++ opakovanie/sada11/uloha2.java
@@ -1,50 +1,43 @@
-package sada11;
+package opakovanie;
 
 import java.util.Scanner;
 
-
-public class uloha2 {
+public class meno {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner x = new Scanner(System.in);
-		System.out.println("Zadaj  svoje meno");
-		String meno = x.nextLine();
-		System.out.println("Tvoje meno je "+ meno);
-		System.out.println("Tvoje meno je dlhe " +meno.length()+" znakov");
-		String samohlasky  = "aeiou";
-		int pocsam = 0;
-		String malemeno = meno.toLowerCase();
-		for (int i = 0; i < malemeno.length(); i++) {
-			if (samohlaska(malemeno.charAt(i),samohlasky)) {
-				++pocsam;
+		System.out.println("Zadaj svoje krstne meno");
+		String meno = x.next();
+		System.out.println(meno);
+		System.out.println("Tvoje krstne meno je dlhe : "+  meno.length());
+		int s = 0;
+		String sam = "aeiou";
+		for (int i = 0; i < meno.length(); i++) {
+			if (obsahuje(sam, meno.charAt(i))) {
+				s++;
 			}
 			
 		}
-		System.out.println("Tvoje meno ma : "+pocsam + " samohlasok");
-		System.out.println("Zadaj pocet opakovani :");
-		int k = x.nextInt();
-		for (int i = 0; i < k; i++) {
-			System.out.print(meno);
-			System.out.print("\n");
-			for (int j = 0; j < (i+1)*meno.length(); j++) {
-				System.out.print(" ");
-				
-			}
-			System.out.print(" ");
+		System.out.println("Tvoje meno obsahuje : " +s+" samohlasok" );
+		System.out.println("Zadaj pocet opakovani: ");
+		int n = x.nextInt();
+		String medzera = "";
+		for (int i = 0; i < n; i++) {
+			System.out.println(medzera+meno);
+			medzera += " ";
 			
 		}
 	}
 
-	private static boolean samohlaska(char c, String samohlasky) {
-		for (int i = 0; i < samohlasky.length(); i++) {
-			if (c == samohlasky.charAt(i)) {
+	private static boolean obsahuje(String sam, char c) {
+		for (int i = 0; i < sam.length(); i++) {
+			if (sam.charAt(i) == c) {
 				return true;
 			}
 			
 		}
 		return false;
 	}
-
 
 }
